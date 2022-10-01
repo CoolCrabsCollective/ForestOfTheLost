098de93a9b4d5a75bbe482140a09ac03ca95a495
Cedric Martens <cedricmartens98@gmail.com>
2022-10-01 13:01:11 -0700

    Every 10 seconds

--- client/res/shaders/spooky.fs
+++ client/res/shaders/spooky.fs
@@ -1,13 +1,18 @@
 uniform sampler2D texture;
 uniform float timeAccumulator;
 
+#define PI 3.141592654
+
 float malformed_euclidean_distance(vec2 v1, vec2 v2, float xStretch) {
     vec2 d = v2 - v1;
     d.x *= xStretch;
 	return sqrt(d.x * d.x + d.y * d.y);
 }
+
 void main()
 {
+    float darkness_interval = 10.0f;
+    float accumulated_time = mod(timeAccumulator * 0.001, darkness_interval);
     vec4 pixel = texture2D(texture, gl_TexCoord[0].xy);
     vec2 center = vec2(0.5, 0.5);
     float speed = 5.0;
@@ -19,6 +24,21 @@ void main()
 
     float distance = malformed_euclidean_distance(center, gl_TexCoord[0].xy, 16.0/9.0);
 
-    gl_FragColor = vec4(pixel.rgb, 1.0 - 2.0 * periodic * distance);
-    //gl_FragColor = vec4(pixel.rgb, value);
+    float darkness_duration = 2.0;
+
+    float darkness_threshold_min = darkness_interval - darkness_duration / 2.0f;
+    float darkness_threshold_max = mod(darkness_interval + darkness_duration / 2.0f, 10.0f);
+
+    float ten_second_darkness_multiplier = (sin(timeAccumulator * 0.001 * 2.0 * PI / darkness_duration - PI/2.0) + 1.0) / 2.0;
+
+    float ambient_darkness = 1.0 - 2.0 * periodic * distance;
+
+    if(accumulated_time < darkness_threshold_max || accumulated_time > darkness_threshold_min)
+    {
+        gl_FragColor = vec4(pixel.rgb, ambient_darkness * ten_second_darkness_multiplier);
+    }
+    else
+    {
+        gl_FragColor = vec4(pixel.rgb, ambient_darkness);
+    }
 }
