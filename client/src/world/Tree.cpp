d07364d6430322d760d8e8bb5c71a09f18ae68d2
William Wells <williamwells2013@hotmail.com>
2022-10-01 18:47:14 -0400

    Finish tree class

--- client/src/world/Tree.cpp
+++ client/src/world/Tree.cpp
@@ -4,10 +4,11 @@
 
 #include "world/Tree.h"
 #include "SFML/Graphics/Texture.hpp"
+#include "GameAssets.h"
 
 Tree::Tree(World& world, const sf::Vector2i& position)
 	: HidingSpot(world, position), Solid() {
-
+    this->sprite = sf::Sprite(*world.getAssets().get(GameAssets::TREE));
 }
 
 bool Tree::isBlocking(sf::Vector2i vec) {
