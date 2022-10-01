03bc7d2847a256f77324f5f3f9aea22b024acd27
Alexander Winter <a.w1nter@hotmail.com>
2022-10-01 16:27:56 -0400

    Bush generation

--- client/src/world/World.cpp
+++ client/src/world/World.cpp
@@ -5,11 +5,18 @@
 #include "world/World.h"
 #include "SFML/System/Vector2.hpp"
 #include "util/SimplexNoise.h"
+#include "world/HidingSpot.h"
+#include "GameAssets.h"
 
 World::World(wiz::AssetLoader& assets)
 		: assets(assets),
 		  player(*this),
-		  terrainMap() {
+		  terrainMap(),
+		  terrain_textures() {
+	terrain_textures[TerrainType::GRASS] = assets.get(GameAssets::GRASS_TERRAIN);
+	terrain_textures[TerrainType::WATER] = assets.get(GameAssets::WATER_TERRAIN);
+	terrain_textures[TerrainType::SAND] = assets.get(GameAssets::SAND_TERRAIN);
+
 	entities.push_back(&player);
 
 	for(int i = -200; i <= 200; i++) {
@@ -25,13 +32,15 @@ World::World(wiz::AssetLoader& assets)
 				terrainMap[sf::Vector2i(i, j)] = TerrainType::WATER;
 			else if(noise < -0.7f)
 				terrainMap[sf::Vector2i(i, j)] = TerrainType::SAND;
+			else if(noise > 0.5f)
+				entities.push_back(new HidingSpot(*this, sf::Vector2i(i, j)));
 		}
 	}
 }
 
-TerrainType World::getTerrainType(sf::Vector2i position) {
+TerrainType World::getTerrainType(sf::Vector2i position) const {
 	if(terrainMap.contains(position))
-		return terrainMap[position];
+		return terrainMap.at(position);
 	return TerrainType::GRASS;
 }
 
@@ -56,6 +65,8 @@ wiz::AssetLoader& World::getAssets() {
 }
 
 bool World::tileOccupied(sf::Vector2i tile, Entity *exclude) {
+
+	/*
 	if(terrainMap[tile] == WATER)
 		return true;
 
@@ -64,7 +75,7 @@ bool World::tileOccupied(sf::Vector2i tile, Entity *exclude) {
         if (occupied) {
             return true;
         }
-    }
+    }*/
 
     return false;
 }
@@ -73,4 +84,31 @@ void World::tick(float delta) {
     for (Entity *entity : entities) {
         entity->tick(delta);
     }
+}
+
+void World::draw(sf::RenderTarget& target, const sf::RenderStates& states) const {
+
+	sf::Vector2f viewSize = {16.0f, 9.0f};
+
+	sf::Vector2i start = getPlayer().getPosition() - sf::Vector2i(static_cast<int>(ceil(viewSize.x / 2.0f)),
+																		static_cast<int>(ceil(viewSize.y / 2.0f))) - sf::Vector2i{1,1};
+	sf::Vector2i end = getPlayer().getPosition() + sf::Vector2i(static_cast<int>(floor(viewSize.x / 2.0f)),
+																	  static_cast<int>(floor(viewSize.y / 2.0f))) + sf::Vector2i{1,1};
+
+	for(int i = start.x; i <= end.x; i++) {
+		for(int j = start.y; j <= end.y; j++) {
+			terrain_sprite.setTexture(*terrain_textures.at(this->getTerrainType({i, j})), true);
+			terrain_sprite.setPosition({static_cast<float>(i), -static_cast<float>(j)});
+			terrain_sprite.setScale({1.0f / terrain_sprite.getTexture()->getSize().x,
+									 1.0f / terrain_sprite.getTexture()->getSize().y});
+			target.draw(terrain_sprite);
+		}
+	}
+
+	for(Entity* entity : getEntities())
+		if(entity->getPosition().x >= start.x
+		&& entity->getPosition().y >= start.y
+		&& entity->getPosition().x <= end.x
+		&& entity->getPosition().y <= end.y)
+			target.draw(*entity);
 }
