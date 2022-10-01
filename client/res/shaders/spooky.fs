b8373bb4dbb0975f305dbcd223d1ff26879023d9
Cedric Martens <cedricmartens98@gmail.com>
2022-10-01 08:28:26 -0700

    Fog

--- client/res/shaders/spooky.fs
+++ client/res/shaders/spooky.fs
@@ -1,20 +1,17 @@
 uniform sampler2D texture;
 
-float euclidean_distance(vec2 v1, vec2 v2) {
+float malformed_euclidean_distance(vec2 v1, vec2 v2, float xStretch) {
     vec2 d = v2 - v1;
+    d.x *= xStretch;
 	return sqrt(d.x * d.x + d.y * d.y);
 }
-
 void main()
 {
     vec4 pixel = texture2D(texture, gl_TexCoord[0].xy);
     vec2 center = vec2(0.5, 0.5);
     float speed = 2.0;
 
-    float distance = euclidean_distance(center, gl_TexCoord[0].xy);
-    //vec2 center = vec2(textureDimensions.x / 2.0, textureDimensions.y / 2.0);
+    float distance = malformed_euclidean_distance(center, gl_TexCoord[0].xy, 16.0/9.0);
 
-    //pixel.b = center.r;
-    //gl_FragColor = gl_Color * pixel;
-    gl_FragColor = vec4(pixel.rgb, 1.0 - distance);
+    gl_FragColor = vec4(pixel.rgb, 1.0 - 2.0 * distance);
 }
