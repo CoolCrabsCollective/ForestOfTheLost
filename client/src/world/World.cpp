d5a3af6dbb0f4371cf6ed5b3f3de5e80077e56fc
William Wells <williamwells2013@hotmail.com>
2022-10-03 13:08:00 -0400

    Fix auto-merge mixup causing build fail

--- client/src/world/World.cpp
+++ client/src/world/World.cpp
@@ -642,7 +642,6 @@ void World::handleMonsterAttack(Monster& monster) {
         if (currentPhase != GamePhase::INITIAL) {
             loadCheckPoint = true;
             timePaused = false;
-        }
 
 			for(Monster* monster : monsters) {
 				removeEntity(monster);
