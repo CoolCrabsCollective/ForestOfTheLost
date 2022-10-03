3cca0271899073d73216d3e7ab7565442128be77
William Wells <williamwells2013@hotmail.com>
2022-10-02 21:43:05 -0400

    Add crying girl and spawn it in world during ghost phase

---
+++ client/include/world/CryingGirl.h
@@ -0,0 +1,17 @@
+//
+// Created by william on 02/10/22.
+//
+
+#ifndef LD51_CLIENT_CRYINGGIRL_H
+#define LD51_CLIENT_CRYINGGIRL_H
+
+#include "NPC.h"
+
+class CryingGirl : public NPC {
+public:
+    CryingGirl(World& world, const sf::Vector2i& position);
+
+    bool activate() override;
+};
+
+#endif //LD51_CLIENT_CRYINGGIRL_H
