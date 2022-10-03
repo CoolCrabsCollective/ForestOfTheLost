b63c2f6592ec5b6468aa58962b938fdfab98ee0a
Adrien Tremblay <adrien@adrientremblay.com>
2022-10-02 21:13:53 -0400

    Update the player walking animation

--- client/src/world/Wraith.cpp
+++ client/src/world/Wraith.cpp
@@ -6,6 +6,6 @@
 #include "world/Wraith.h"
 
 Wraith::Wraith(World &world, sf::Vector2i position) : Monster(world, position, world.getAssets().get(GameAssets::INVISIBLE), world.getAssets().get(GameAssets::WRAITH)){
-    attackMessage = "Cold. So cold. A piercing scream fills your ears. A wraith has killed you.";
+    attackMessage = "Cold. Why am I so cold? A piercing scream fills your ears. Your own.  A wraith has killed you.";
     scale = 4.0f;
 }
